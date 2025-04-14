c9f9649d5577071d2c890df338d1df74825c05aa
Ivanchodev <vargasivan203@gmail.com>
2025-04-14 14:33:06 -0400

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    apps: [
-      {
-        name: "simple-node-app",
-        script: "./app.js",
-        instances: 1,
-        autorestart: true,
-        watch: false
-      }
-    ]
-  };
-  
+  apps: [
+    {
+      name: "simple-node-app",
+      script: "./app.js",
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false
+    }
+  ]
+};
