d896c53beb0b98a2391d6168761fabe193a2f2f2
Ivanchodev <vargasivan203@gmail.com>
2025-04-14 14:55:03 -0400

    arreglando githubaction

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Actualizando el repositorio de ivargas');
+  res.send('Actualizando el repositorio de ivargas58');
 });
 
 app.listen(PORT, () => {
