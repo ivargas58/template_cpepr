f40eedc13337d23540ec4c6c0a9b22a0bb3caf96
Ivanchodev <vargasivan203@gmail.com>
2025-04-16 19:57:23 -0400

    volviendo a intentar

--- testdb.js
+++ testdb.js
@@ -3,9 +3,9 @@ const pool = require('./db');
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
-    console.error('❌ Error al conectar con la base de datos:', err);
+    console.error('Error al conectar con la base de datos:', err);
   } else {
-    console.log('✅ Conexión exitosa:', res.rows[0]);
+    console.log('Conexión exitosa:', res.rows[0]);
   }
 
   pool.end(); // Cierra la conexión
