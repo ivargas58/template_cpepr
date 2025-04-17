d3c59e67fbfee77da7e9064a9482a323fe28efe5
Ivanchodev <vargasivan203@gmail.com>
2025-04-17 19:17:37 -0400

    Verificando

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/jkx', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
