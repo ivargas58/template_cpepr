b1b51de6b207b0bcce7d5e9d74628841f600528a
Ivanchodev <vargasivan203@gmail.com>
2025-04-21 10:31:04 -0400

    added servicios

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/servicios', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'servicios.html'));
+});
+
 app.get('/jkx', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
