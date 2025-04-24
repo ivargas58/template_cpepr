e626ab2d91401fb6400a0d7d7e6674e906fe3be9
Ivanchodev <vargasivan203@gmail.com>
2025-04-24 15:08:39 -0400

    Intentando conectar a EC2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const pool = require('./db'); 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
