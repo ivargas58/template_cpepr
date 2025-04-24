462fd7a8997c473c0bd8823d73a290446ca70ff9
Ivanchodev <vargasivan203@gmail.com>
2025-04-24 14:27:57 -0400

    Comenzando nuevo proyecto

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const pool = require('./db'); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
