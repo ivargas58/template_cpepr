3ba43072b3c9db1b4f495499cb174fcf48b64cb8
Ivanchodev <vargasivan203@gmail.com>
2025-04-24 16:25:28 -0400

    verificando que no haya errores

--- db.js
+++ db.js
@@ -2,6 +2,14 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// Agrega logs para verificar que las variables están correctamente cargadas
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD tipo:', typeof process.env.DB_PASSWORD);
+console.log('DB_PASSWORD valor:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
